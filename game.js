19a999f0ba7531b17f6fd5b53c4ecc8fb6158ca2
Ben Moeller <benlynnmoeller@gmail.com>
2021-05-14 01:21:31 -0500

    5/14/2021 changes - 1:21 AM CT

--- game.js
+++ game.js
@@ -2,21 +2,45 @@ let canvas;
 let context;
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("got here");
-    canvas = document.querySelector("#myCanvas");
-    context = canvas.getContext("2d");
-    console.log("ahhhhhh");
-    main_menu();
+  console.log("got here");
+  canvas = document.querySelector("#myCanvas");
+  context = canvas.getContext("2d");
+  main_menu();
 })
 
+document.addEventListener("click", game_click);
+
+function game_click(e) {
+  console.log("click recieved");
+  
+}
+
 function main_menu() {
-    var button = document.createElement("button");
-    button.innerHTML = "Play Game";
-    var body = document.getElementsByTagName("body")[0];
-    body.appendChild(button);
-    button.addEventListener("click", () => {
-        console.log("hey");
-    })
+  context.fillStyle = '#000000';
+  context.font = '100pt Candara';
+  context.fillText("Aim Test", 430, 250);
+  var button = document.createElement("button");
+  button.innerHTML = "Play Game";
+  var body = document.getElementsByTagName("body")[0];
+  body.appendChild(button);
+  button.addEventListener("click", () => {
+      context.clearRect(0,0,canvas.width,canvas.height);
+      body.removeChild(button);
+      print_instructions();
+      print_game();
+  })
+}
+
+function print_instructions() {
+  context.font = '60pt Candara'
+  context.fillText("In order to play please", 300, 100);
+  context.fillText("click all of the objects", 310, 250);
+  context.fillText("on the screen", 430, 400);
+  setTimeout(() => context.clearRect(0,0,canvas.width,canvas.height), 4000);
+}
+
+function print_game() {
+
 }
 
 function tick() {
